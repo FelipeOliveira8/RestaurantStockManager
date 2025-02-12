adde50f0581bd9562b1cf9b4223819ecadd30c13
Felipe Oliveira <fc0669493@gmail.com>
2025-02-12 20:51:52 -0300

    HomePage frontend finalization

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,47 @@ import "./HomePage.css"
 function HomePage(){
     return(
         <>
-            <h1>Home Page</h1>
+            <section>
+                <div className="container">
+                    <div className="info">
+                        <div className="order">
+                            <button className="Registers">
+                                Registros
+                            </button>
+                            <button className="openNewOrder">
+                                Abrir Novo Pedido
+                            </button>
+                        </div>
+                        <div className="financialResults">
+                            <div>
+                                <p>Resultados:</p>
+                                <p>+R$100.000</p>
+                            </div>
+                            <div>
+                                <p>Faturamento:</p>
+                                <p>R$400.000</p>
+                            </div>
+                            <div>
+                                <p>Custos:</p>
+                                <p>R$300.000</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="orders">
+                        <div className="orderBox">
+                            <div>
+                                <a href="#">Pedido1</a>
+                                <p>Bruno</p>
+                                <h3>24:35</h3>
+                            </div>
+                            <div>
+                                <button>Finalizar</button>
+                                <button>Cancelar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 } 
